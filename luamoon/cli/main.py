11328138f8c019d66d08d4868395b3bd514a22c5
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-01-24 21:57:52 +0300

    added more options

--- luamoon/cli/main.py
+++ luamoon/cli/main.py
@@ -7,9 +7,13 @@ def get_arg_parser():
     parser.add_argument('-v', '--version', action='version', version=VERSION)
     # parser.add_argument('-h', '--help', action='help', help=HELP['general'])
 
-    subparsers = parser.add_subparsers(dest='subparsers')
+    subparsers = parser.add_subparsers(dest='command')
 
-    subparsers.add_parser('init')
+    init_parser = subparsers.add_parser('init')
+    init_parser.add_argument('-l', '--lua', help=HELP['init-lua'])
+    # todo: support choosing lua version
+    init_parser.add_argument('-e', '--env', help=HELP['init-env'])
+    # todo: venv name is not changing fix that.
 
     add_parser = subparsers.add_parser('add')
     add_parser.add_argument('package_name')
--- luamoon/cli/strings.py
+++ luamoon/cli/strings.py
@@ -3,6 +3,8 @@ VERSION = '0.1.0'
 DESCRIPTION = 'LuaMoon Package Manager'
 
 HELP = {
-    'general': ''
+    'general': '',
+    'init-lua': 'Choose the desired lua version (Note: That version must be installed on the system)',
+    'init-env': 'Change the environment name. (default: .vmoon)',
 }
 
