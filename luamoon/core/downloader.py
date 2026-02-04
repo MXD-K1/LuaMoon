a2d7b47e4af66de875466ff4d7db3e83e9b79115
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-05 00:12:47 +0300

    split the download process from the core functionality + added retires and timeout and extract .zip and .tar (.gz, .bz2, .xz) files

--- luamoon/core/downloader.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def download_pkg(url, path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(path, 'wb') as f:
-            f.write(response.content)
-    else:
-        pass  # todo: Add appropriate error message in the cli
---
+++ luamoon/fetch/download.py
@@ -0,0 +1,23 @@
+import os
+
+import requests
+
+RETRIES = 4
+TIMEOUT = 15
+
+def get_url_content(url: str, timeout=TIMEOUT, retries=RETRIES):
+    response = requests.get(url, timeout=timeout)
+    for _ in range(retries):
+        if response.status_code == 200:
+            return response.content
+    else:
+        raise Exception(f"Could not fetch url '{url}'")
+        # todo: Add appropriate error message in the cli
+
+# noinspection PyTypeChecker
+def download_pkg(url: str, pkg_name: str, pkg_version: str, path: str):
+    response_bytes = get_url_content(url.format(version=pkg_version))
+    full_path = os.path.join(path, pkg_name + '.zip')
+
+    with open(full_path, "wb") as zip_f:
+        zip_f.write(response_bytes)
