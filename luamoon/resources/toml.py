917fd789b5ddb8abca3fdbac309896ec93630abb
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-05 21:05:14 +0300

    ...

--- luamoon/resources/toml.py
+++ luamoon/resources/toml.py
@@ -8,11 +8,39 @@ project_headers = {
     "project": {
         "name": "myproject",
         "version": "0.1.0",
-        "lua_version": "5.1"
+        "description": "",
+        "author": "",
+        "license": "",
+    },
+    "environment": {
+        "type": "",
+        "lua_version": "5.1",
+        "entry_point": ""
     },
     "dependencies": {}
 }
 
+lib_headers = {
+    "project": {
+        "name": "myproject",
+        "version": "0.1.0",
+        "description": "",
+        "author": "",
+        "license": ""
+    },
+    "environment": {
+        "type": "",
+        "lua_version": "5.1",
+    },
+    "dependencies": {}
+}
+
+# todo: fill the missing fields
+
 def create_project_toml():
     with open(os.path.join(path, 'luaproject.toml'), 'w') as toml_file:
         toml_file.write(toml.dumps(project_headers))
+
+def create_lib_toml():
+    with open(os.path.join(path, 'lualib.toml'), 'w') as toml_file:
+        toml_file.write(toml.dumps(lib_headers))
