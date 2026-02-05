ace517ca7a875fad42a5d7b766b36144d2171306
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-05 21:26:20 +0300

    ...

--- luamoon/__init__.py
+++ luamoon/__init__.py
@@ -1,7 +1,8 @@
 import os
 
 PATH = os.environ.copy()
+HOMEPATH = os.path.expanduser("~")
 
 # noinspection SpellCheckingInspection
-CACHE_DIR = os.path.join(PATH['HOMEDRIVE'], PATH['HOMEPATH'], '.luamoon')
+CACHE_DIR = os.path.join(HOMEPATH, '.luamoon')
 CONFIG_FILE_PATH = os.path.join(CACHE_DIR, 'config.toml')  # global config
