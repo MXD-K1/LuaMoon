7ce0c751fb3b3b7efdc2f1648eca8f4134c6c732
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-05 21:33:02 +0300

    ...

--- luamoon/core/commands/init.py
+++ luamoon/core/commands/init.py
@@ -1,10 +1,9 @@
 import os
-import json
 
 from luamoon.core import *
 from luamoon.core.main import get_lua_version
 from luamoon.resources.lockfile import create_lockfile_headers, create_lockfile
-from luamoon.resources.toml import create_project_toml
+from luamoon.resources.toml import create_project_toml, create_lib_toml
 
 
 def init_project():
@@ -35,4 +34,28 @@ def init_project():
     create_lockfile(headers_)
 
 def init_lib():
-    pass
+    # create dirs and files
+    try:
+        os.mkdir(venv_path)
+        os.mkdir(os.path.join(path, 'src'))
+        os.mkdir(os.path.join(path, venv_name, 'bin'))
+        os.mkdir(os.path.join(path, venv_name, 'packages'))
+        os.mkdir(os.path.join(path, venv_name, 'include'))
+
+        with open(os.path.join(path, 'README.md'), 'w'): pass
+        with open(os.path.join(path, 'src', 'main.lua'), 'w') as f:
+            f.write('print("Hello World")')
+
+        create_lib_toml()
+
+    except FileExistsError:
+        pass  # todo: Add appropriate error message in the cli
+        # todo: ask for overriding
+
+    # todo: support existing projects
+
+    # todo: Ship the binary in the venv dir
+    # todo: Ship the lua binary in the venv dir
+
+    headers_ = create_lockfile_headers(get_lua_version())
+    create_lockfile(headers_)
