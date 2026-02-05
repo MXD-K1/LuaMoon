5ae5301f1ac24530bd1860b4c97e4a9d727e6982
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-05 21:47:52 +0300

    added remove_package

--- luamoon/core/commands/pkg_manager_ops.py
+++ luamoon/core/commands/pkg_manager_ops.py
@@ -1,14 +1,14 @@
+import os
 import json
 
-from luamoon.core import index_file_path
-from luamoon.resources.lockfile import add_package_data
+from luamoon.core import index_file_path, package_path
+from luamoon.resources.lockfile import add_package_data, remove_package_data
 
 def add_package(package_name):
     with open(index_file_path, 'r') as index_file:
         data = json.load(index_file)
     try:
-        pkg_data = data[package_name]
-        pkg_data = pkg_data[0]  # todo: resolve the appropriate version
+        pkg_data = data[package_name]  # todo: resolve the appropriate version
     except KeyError:
         return  # todo: Add appropriate error message in the cli
 
@@ -21,7 +21,11 @@ def add_package(package_name):
     add_package_data(package_name, pkg_data)
 
 def remove_package(package_name):
-    pass
+    remove_package_data(package_name)
+    try:
+        os.remove(os.path.join(package_path, package_name))
+    except FileNotFoundError:
+        return  # todo: Add proper cli error message
 
 def update_package(package_name):
     pass
