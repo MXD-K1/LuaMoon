936345ea623fa6014883961d77a52911bb984b06
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-07 00:27:00 +0300

    added support for constraints so that specific version can be requested. TODO: use that in the project

---
+++ luamoon/resolver/constraints.py
@@ -0,0 +1,52 @@
+import re
+import operator
+
+OPS_MAP = {
+    '==': operator.eq,
+    '>=': operator.ge,
+    '<=': operator.le,
+    '>': operator.gt,
+    '<': operator.lt
+}
+
+PATTERN = re.compile(
+    r'([A-Za-z_-][A-Za-z0-9_-]*)' +              # package name
+    rf'({"|".join(re.escape(op) for op in OPS_MAP.keys())})' +  # operator
+    r'(\d+(?:\.\d+)*)$'                           # version (no trailing dot)
+)
+
+def parse_constraint(constraint_str) -> dict[str , str]:
+    if is_valid_constraint(constraint_str):
+        match_ = re.match(PATTERN, constraint_str)
+        return {
+            'package_name': match_.group(1),
+            'operator': match_.group(2),
+            'version': match_.group(3)
+        }
+    else:
+        # todo: raise an error
+        return {}
+
+def matches(version, constraint):
+    _, op, c_ver = parse_constraint(constraint).values()
+    norm_version = tuple(normalize_version(version).split('.'))
+    norm_c_ver = tuple(normalize_version(c_ver).split('.'))
+    return OPS_MAP[op](norm_version, norm_c_ver)
+
+def is_valid_constraint(constraint_str):
+    match_ = re.match(PATTERN, constraint_str)
+    return match_ is not None
+
+def normalize_version(version):
+    parts = version.split('.')
+
+    for i, part in enumerate(parts):
+        parts[i] = str(int(part))  # remove leading zeros
+
+    if len(parts) > 3:
+        return '.'.join(parts[:3])
+    elif len(parts) == 3:
+        return '.'.join(parts)
+    else:
+        parts.extend(['0'] * (3 - len(parts)))
+        return '.'.join(parts)
