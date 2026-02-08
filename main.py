44d2e60dc7a8ee8b8e29d9a4e760a6174b7f3f87
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-08 17:49:48 +0300

    ...

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from luamoon.cli.main import get_arg_parser
 from luamoon.core.main import *
 from luamoon.core.commands import *
 
+NOT_IMPLEMENTED_MSG = "Sorry, this feature isn’t available yet :)"
 
 def main():
     parser = get_arg_parser()
@@ -12,7 +13,11 @@ def main():
             change_lua_version(args.lua)
         if args.env:
             change_venv_name(args.env)
-        init(args.type_, args.runtime)
+
+        if args.runtime == 'lua':
+            init(args.type_, args.runtime)
+        else:
+            print(NOT_IMPLEMENTED_MSG)
 
     elif args.command == 'add':
         add_package(args.package_name)
@@ -22,7 +27,14 @@ def main():
         remove_package(args.package_name)
     elif args.command == 'list':
         list_packages()
+    elif args.command == 'search':
+        print(NOT_IMPLEMENTED_MSG)
+    elif args.command == 'publish':
+        print(NOT_IMPLEMENTED_MSG)
+    elif args.command == 'config':
+        print(NOT_IMPLEMENTED_MSG)
+    elif args.command == 'run':
+        print(NOT_IMPLEMENTED_MSG)
 
 if __name__ == '__main__':
     main()
-
