2f28bec96b79b5c1d6d204c08f6ed0233bf116ff
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-08 20:11:36 +0300

    added basic color support.

--- luamoon/colors.py
+++ luamoon/colors.py
@@ -2,7 +2,11 @@ from enum import Enum
 
 
 class Color(Enum):
-    CYAN = "#00BFFF"
+    GREEN = "#00FF00"  # success
+    CYAN = "#00BFFF"  # info
+    YELLOW = "#FFFF00"  # warning
+    RED = "#FF0000"  # error/failure
+    MAGENTA = "#FF00FF"  # debugging
 
 def color_text(color, text):
     return f"[{color.value}]{text}[/{color.value}]"
