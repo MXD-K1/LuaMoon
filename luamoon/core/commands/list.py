76fcc7ce8f21ea3c5c7ca9a10159fb8303be0372
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-02-12 23:34:02 +0300

    added list command

--- luamoon/core/commands/list.py
+++ luamoon/core/commands/list.py
@@ -1,2 +1,7 @@
+from luamoon.resources.toml import read_toml
+from luamoon.core import path
+
 def list_packages():
-    pass
+    data = read_toml(path)
+    for entry in data['dependencies'].items():
+        print(f"{entry[0]}=={entry[1]}")
